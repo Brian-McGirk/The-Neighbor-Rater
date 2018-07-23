28183b9c1ee105a79a6c24326ea32be16158d998
Brian McGirk <bmcgirk2014@gmail.com>
2018-07-23 10:56:46 -0500

    Added functionality so that when a user enters apartment info it will save to the database

--- src/main/java/TheNeighborRater/controllers/ApartmentController.java
+++ src/main/java/TheNeighborRater/controllers/ApartmentController.java
@@ -1,7 +1,12 @@
 package TheNeighborRater.controllers;
 
+import TheNeighborRater.models.Apartment;
+import TheNeighborRater.models.data.ApartmentDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,16 +15,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("apartment")
 public class ApartmentController {
 
+    @Autowired
+    private ApartmentDao apartmentDao;
+
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddForm(Model model){
 
         model.addAttribute("title", "Add");
+        model.addAttribute(new Apartment());
 
         return "apartment/index";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddForm(Model model){
+    public String processAddForm(Model model, @ModelAttribute Apartment apartment, Errors errors){
+
+        if(errors.hasErrors()){
+
+            model.addAttribute("title", "Add");
+            model.addAttribute(apartment);
+
+            return "apartment/index";
+        }
+
+        apartmentDao.save(apartment);
 
         model.addAttribute("title", "Add");
 
